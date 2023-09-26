73259d7867c2ba034bef4708f92500493be9f4a9
Muhammad Mosyyab Khan <mosyyabkhan@gmail.com>
2023-09-26 15:21:00 +0100

    clean

--- Assets/_Scripts/ScriptableObjects/_Scripts/CharactersManagment.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerManager", menuName = "CharacterManager")]
-public class CharactersManagment : ScriptableObject
-{
-    public Characters[] Players;
-}
